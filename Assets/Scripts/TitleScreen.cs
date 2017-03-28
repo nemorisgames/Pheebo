d5ec5c5e45610ee678b72a84ec4289a856489ebb
nemorisgames <contacto@nemorisgames.com>
2017-03-28 17:40:07 -0300

    corrections

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void playGame(){
+		SceneManager.LoadScene ("Park");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
